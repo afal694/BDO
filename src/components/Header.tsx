fabaf60fc2e5582e466651c91adafbbb2c6bbf4a
Andres <and.694@gmail.com>
2021-12-25 18:02:45 -0500

    header queries

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,10 @@ import Logo from "../assets/logo.png";
 const AvatarComponent = () => {
 	return (
 		<div className="avatar-component">
-			<img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+			<img
+				src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+				alt="avatar"
+			/>
 		</div>
 	);
 };
@@ -20,6 +23,35 @@ const Header = ({
 		isNotMobile: boolean;
 	};
 }) => {
+	if (queries.isDesktop) {
+		return (
+			<header className="d-flex flex-row justify-content-between">
+				<div className="logo">
+					<img src={Logo} alt="logo" />
+				</div>
+				<div className="last-button">
+					<AvatarComponent />
+				</div>
+			</header>
+		);
+	}
+
+	if (queries.isTablet || queries.isMobile) {
+		return (
+			<header className="d-flex flex-row justify-content-between">
+				<div className="first-button">
+					<Icon.ArrowLeft />
+				</div>
+				<div className="logo">
+					<img src={Logo} alt="logo" />
+				</div>
+				<div className="last-button">
+					<Icon.List />
+				</div>
+			</header>
+		);
+	}
+
 	return (
 		<header>
 			<div className="first-button">
@@ -29,7 +61,6 @@ const Header = ({
 				<img src={Logo} alt="logo" />
 			</div>
 			<div className="last-button">
-				{/* <Icon.List /> */}
 				<AvatarComponent />
 			</div>
 		</header>
