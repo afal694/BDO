ba8e5ac0c4409d3137e7d903d111d4484db9fa5c
Andres <and.694@gmail.com>
2021-12-25 17:05:25 -0500

    fix CustomAlert icon

--- src/components/CustomAlert.tsx
+++ src/components/CustomAlert.tsx
@@ -2,7 +2,7 @@ const CustomAlert = ({ message }: { message: string }) => {
 	return (
 		<div className="custom-alert alert alert-warning" role="alert">
 			<div className="icon">
-				<i />
+				<i className="bi bi-info-circle" />
 			</div>
 			<div className="divider"></div>
 			<div className="message">{message}</div>
